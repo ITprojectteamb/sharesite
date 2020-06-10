e6a44b156088e033698422f17abfc0963cb95768
ITprojectteamb <natsukiusanc2004@gmail.com>
2020-06-10 18:53:13 +0900

    6/10 19時

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/givelist/', views.give_list, name='give_list'),
-    path('post/wantlist/', views.want_list, name='want_list'),
-    path('post/givenew/', views.give_new, name='give_new'),
-    path('post/wantnew/', views.want_new, name='want_new'),
-    path('post/profilenew/', views.profile_new, name='profile_new'),
-    path('post/givedetail/<int:pk>/', views.give_detail, name='give_detail'),
-    path('post/wantdetail/<int:pk>/', views.want_detail, name='want_detail'),
+    path('givelist/', views.give_list, name='give_list'),
+    path('wantlist/', views.want_list, name='want_list'),
+    path('givenew/', views.give_new, name='give_new'),
+    path('wantnew/', views.want_new, name='want_new'),
+    path('profilenew/', views.profile_new, name='profile_new'),
+    path('givedetail<int:pk>/', views.give_detail, name='give_detail'),
+    path('wantdetail<int:pk>/', views.want_detail, name='want_detail'),
 ]
