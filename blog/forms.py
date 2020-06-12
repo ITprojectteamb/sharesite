545758018e4f292831d9e72ec11fdcc35b82cbcc
Kensuke Takahashi <ktakahashi@skylight.co.jp>
2020-06-12 14:16:10 +0900

    kensuke06121416

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,22 @@
 from django import forms
 
-from .models import Post
+from .models import Post,Give,Want
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
+        fields = ('title', 'text',)
+
+
+class GiveForm(forms.ModelForm):
+
+    class Meta:
+        model = Give
+        fields = ('title', 'text',)
+
+class WantForm(forms.ModelForm):
+
+    class Meta:
+        model = Want
         fields = ('title', 'text',)
