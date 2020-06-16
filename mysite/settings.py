b90020d6b518bd8a52d6efe8a67e4b0aed07d45f
jyo kanai <lazystone.git-20@rakuten.jp>
2020-06-16 12:48:38 +0900

    メール自動配信

--- mysite/settings.py
+++ mysite/settings.py
@@ -160,4 +160,13 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
 # デフォルトのメール送信元を設定
 DEFAULT_FROM_EMAIL = 'admin@example.com'
 # メール送信先（コンソール）
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 't.local.k@gmail.com'
+EMAIL_HOST_PASSWORD = 'qgywnbcqshimgfiy'
