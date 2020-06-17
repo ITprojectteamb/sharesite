9da5bdbfd9de60c232d57bafa437aded4d3bda9f
yuma <yuma_watanabe@hq.skylight.co.jp>
2020-06-17 20:02:17 +0900

    渡辺3

--- blog/views.py
+++ blog/views.py
@@ -190,3 +190,27 @@ class GiveUpdateView(LoginRequiredMixin, generic.UpdateView):
         messages.error(self.request, "投稿を更新できませんでした。")
         return super().form_invalid(form)
 
+class WantDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Want
+    template_name = 'want_delete.html'
+    success_url = reverse_lazy('want_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "投稿を削除しました。")
+        return super().delete(request, *args, **kwargs)
+
+class WantUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Want
+    template_name = 'want_update.html'
+    form_class = WantCreateForm
+
+    def get_success_url(self):
+        return reverse_lazy('want_detail', kwargs={'pk': self.kwargs['pk']})
+
+    def form_valid(self, form):
+        messages.success(self.request, '投稿を更新しました。')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "投稿を更新できませんでした。")
+        return super().form_invalid(form)
